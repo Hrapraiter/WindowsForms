bdc10af59e7491718e5792116bdf48c8a8e3d658
unknown <223995225+Hrapraiter@users.noreply.github.com>
2026-01-31 01:11:18 +0700

    Topmost , Sys tray db_click ,... DONE

--- Clock/MainForm.Designer.cs
+++ Clock/MainForm.Designer.cs
@@ -31,11 +31,6 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.labelTime = new System.Windows.Forms.Label();
-            this.timer = new System.Windows.Forms.Timer(this.components);
-            this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
-            this.checkBoxShowWeekDay = new System.Windows.Forms.CheckBox();
-            this.buttonHideControls = new System.Windows.Forms.Button();
-            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.tsmiTopmost = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiShowControls = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,13 +41,24 @@
             this.tsmiFont = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiColor = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiForegraundColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiForegroundColor_ControlText = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiForegroundColor_Crimson = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiForegroundColor_DarkKhaki = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmiBackgroundColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBackgroundColor_MenuHighlight = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBackgroundColor_YellowGreen = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiBackgroundColor_Orange = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmiaAlarms = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmiAutorun = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
             this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxShowDate = new System.Windows.Forms.CheckBox();
+            this.checkBoxShowWeekDay = new System.Windows.Forms.CheckBox();
+            this.buttonHideControls = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -68,51 +74,6 @@
             this.labelTime.TabIndex = 0;
             this.labelTime.Text = "CurrentTime";
             this.labelTime.DoubleClick += new System.EventHandler(this.labelTime_DoubleClick);
-            // 
-            // timer
-            // 
-            this.timer.Enabled = true;
-            this.timer.Tick += new System.EventHandler(this.timer_Tick);
-            // 
-            // checkBoxShowDate
-            // 
-            this.checkBoxShowDate.AutoSize = true;
-            this.checkBoxShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBoxShowDate.Location = new System.Drawing.Point(12, 266);
-            this.checkBoxShowDate.Name = "checkBoxShowDate";
-            this.checkBoxShowDate.Size = new System.Drawing.Size(194, 41);
-            this.checkBoxShowDate.TabIndex = 1;
-            this.checkBoxShowDate.Text = "Show date";
-            this.checkBoxShowDate.UseVisualStyleBackColor = true;
-            // 
-            // checkBoxShowWeekDay
-            // 
-            this.checkBoxShowWeekDay.AutoSize = true;
-            this.checkBoxShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.checkBoxShowWeekDay.Location = new System.Drawing.Point(12, 324);
-            this.checkBoxShowWeekDay.Name = "checkBoxShowWeekDay";
-            this.checkBoxShowWeekDay.Size = new System.Drawing.Size(278, 41);
-            this.checkBoxShowWeekDay.TabIndex = 2;
-            this.checkBoxShowWeekDay.Text = "Show Week Day";
-            this.checkBoxShowWeekDay.UseVisualStyleBackColor = true;
-            // 
-            // buttonHideControls
-            // 
-            this.buttonHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.buttonHideControls.Location = new System.Drawing.Point(12, 371);
-            this.buttonHideControls.Name = "buttonHideControls";
-            this.buttonHideControls.Size = new System.Drawing.Size(380, 67);
-            this.buttonHideControls.TabIndex = 3;
-            this.buttonHideControls.Text = "Hide controls";
-            this.buttonHideControls.UseVisualStyleBackColor = true;
-            this.buttonHideControls.Click += new System.EventHandler(this.buttonHideControls_Click);
-            // 
-            // notifyIcon
-            // 
-            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
-            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
-            this.notifyIcon.Text = "Clock PV_522";
-            this.notifyIcon.Visible = true;
             // 
             // contextMenuStrip
             // 
@@ -144,7 +105,9 @@
             // 
             // tsmiShowControls
             // 
+            this.tsmiShowControls.Checked = true;
             this.tsmiShowControls.CheckOnClick = true;
+            this.tsmiShowControls.CheckState = System.Windows.Forms.CheckState.Checked;
             this.tsmiShowControls.Name = "tsmiShowControls";
             this.tsmiShowControls.Size = new System.Drawing.Size(240, 32);
             this.tsmiShowControls.Text = "Show controls";
@@ -190,15 +153,76 @@
             // 
             // tsmiForegraundColor
             // 
+            this.tsmiForegraundColor.BackColor = System.Drawing.SystemColors.Control;
+            this.tsmiForegraundColor.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiForegroundColor_ControlText,
+            this.tsmiForegroundColor_Crimson,
+            this.tsmiForegroundColor_DarkKhaki});
             this.tsmiForegraundColor.Name = "tsmiForegraundColor";
             this.tsmiForegraundColor.Size = new System.Drawing.Size(270, 34);
             this.tsmiForegraundColor.Text = "Foregraund color";
             // 
+            // tsmiForegroundColor_ControlText
+            // 
+            this.tsmiForegroundColor_ControlText.BackColor = System.Drawing.SystemColors.ControlText;
+            this.tsmiForegroundColor_ControlText.Checked = true;
+            this.tsmiForegroundColor_ControlText.CheckOnClick = true;
+            this.tsmiForegroundColor_ControlText.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiForegroundColor_ControlText.Name = "tsmiForegroundColor_ControlText";
+            this.tsmiForegroundColor_ControlText.Size = new System.Drawing.Size(270, 34);
+            this.tsmiForegroundColor_ControlText.Text = " ";
+            // 
+            // tsmiForegroundColor_Crimson
+            // 
+            this.tsmiForegroundColor_Crimson.BackColor = System.Drawing.Color.Crimson;
+            this.tsmiForegroundColor_Crimson.CheckOnClick = true;
+            this.tsmiForegroundColor_Crimson.Name = "tsmiForegroundColor_Crimson";
+            this.tsmiForegroundColor_Crimson.Size = new System.Drawing.Size(270, 34);
+            this.tsmiForegroundColor_Crimson.Text = " ";
+            // 
+            // tsmiForegroundColor_DarkKhaki
+            // 
+            this.tsmiForegroundColor_DarkKhaki.BackColor = System.Drawing.Color.DarkKhaki;
+            this.tsmiForegroundColor_DarkKhaki.CheckOnClick = true;
+            this.tsmiForegroundColor_DarkKhaki.Name = "tsmiForegroundColor_DarkKhaki";
+            this.tsmiForegroundColor_DarkKhaki.Size = new System.Drawing.Size(270, 34);
+            this.tsmiForegroundColor_DarkKhaki.Text = " ";
+            // 
             // tsmiBackgroundColor
             // 
+            this.tsmiBackgroundColor.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiBackgroundColor_MenuHighlight,
+            this.tsmiBackgroundColor_YellowGreen,
+            this.tsmiBackgroundColor_Orange});
             this.tsmiBackgroundColor.Name = "tsmiBackgroundColor";
             this.tsmiBackgroundColor.Size = new System.Drawing.Size(270, 34);
             this.tsmiBackgroundColor.Text = "Background color";
+            // 
+            // tsmiBackgroundColor_MenuHighlight
+            // 
+            this.tsmiBackgroundColor_MenuHighlight.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.tsmiBackgroundColor_MenuHighlight.Checked = true;
+            this.tsmiBackgroundColor_MenuHighlight.CheckOnClick = true;
+            this.tsmiBackgroundColor_MenuHighlight.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.tsmiBackgroundColor_MenuHighlight.Name = "tsmiBackgroundColor_MenuHighlight";
+            this.tsmiBackgroundColor_MenuHighlight.Size = new System.Drawing.Size(119, 34);
+            this.tsmiBackgroundColor_MenuHighlight.Text = " ";
+            // 
+            // tsmiBackgroundColor_YellowGreen
+            // 
+            this.tsmiBackgroundColor_YellowGreen.BackColor = System.Drawing.Color.YellowGreen;
+            this.tsmiBackgroundColor_YellowGreen.CheckOnClick = true;
+            this.tsmiBackgroundColor_YellowGreen.Name = "tsmiBackgroundColor_YellowGreen";
+            this.tsmiBackgroundColor_YellowGreen.Size = new System.Drawing.Size(119, 34);
+            this.tsmiBackgroundColor_YellowGreen.Text = " ";
+            // 
+            // tsmiBackgroundColor_Orange
+            // 
+            this.tsmiBackgroundColor_Orange.BackColor = System.Drawing.Color.Orange;
+            this.tsmiBackgroundColor_Orange.CheckOnClick = true;
+            this.tsmiBackgroundColor_Orange.Name = "tsmiBackgroundColor_Orange";
+            this.tsmiBackgroundColor_Orange.Size = new System.Drawing.Size(119, 34);
+            this.tsmiBackgroundColor_Orange.Text = " ";
             // 
             // toolStripSeparator3
             // 
@@ -230,9 +254,58 @@
             // 
             // tsmiExit
             // 
+            this.tsmiExit.CheckOnClick = true;
             this.tsmiExit.Name = "tsmiExit";
             this.tsmiExit.Size = new System.Drawing.Size(240, 32);
             this.tsmiExit.Text = "Exit";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // checkBoxShowDate
+            // 
+            this.checkBoxShowDate.AutoSize = true;
+            this.checkBoxShowDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxShowDate.Location = new System.Drawing.Point(12, 266);
+            this.checkBoxShowDate.Name = "checkBoxShowDate";
+            this.checkBoxShowDate.Size = new System.Drawing.Size(194, 41);
+            this.checkBoxShowDate.TabIndex = 1;
+            this.checkBoxShowDate.Text = "Show date";
+            this.checkBoxShowDate.UseVisualStyleBackColor = true;
+            this.checkBoxShowDate.CheckedChanged += new System.EventHandler(this.checkBoxShowDate_CheckedChanged);
+            // 
+            // checkBoxShowWeekDay
+            // 
+            this.checkBoxShowWeekDay.AutoSize = true;
+            this.checkBoxShowWeekDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxShowWeekDay.Location = new System.Drawing.Point(12, 324);
+            this.checkBoxShowWeekDay.Name = "checkBoxShowWeekDay";
+            this.checkBoxShowWeekDay.Size = new System.Drawing.Size(278, 41);
+            this.checkBoxShowWeekDay.TabIndex = 2;
+            this.checkBoxShowWeekDay.Text = "Show Week Day";
+            this.checkBoxShowWeekDay.UseVisualStyleBackColor = true;
+            this.checkBoxShowWeekDay.CheckedChanged += new System.EventHandler(this.checkBoxShowWeekDay_CheckedChanged);
+            // 
+            // buttonHideControls
+            // 
+            this.buttonHideControls.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonHideControls.Location = new System.Drawing.Point(12, 371);
+            this.buttonHideControls.Name = "buttonHideControls";
+            this.buttonHideControls.Size = new System.Drawing.Size(380, 67);
+            this.buttonHideControls.TabIndex = 3;
+            this.buttonHideControls.Text = "Hide controls";
+            this.buttonHideControls.UseVisualStyleBackColor = true;
+            this.buttonHideControls.Click += new System.EventHandler(this.buttonHideControls_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Clock PV_522";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
             // 
             // MainForm
             // 
@@ -284,6 +357,12 @@
         private System.Windows.Forms.ToolStripMenuItem tsmiAutorun;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
         private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmiForegroundColor_ControlText;
+        private System.Windows.Forms.ToolStripMenuItem tsmiForegroundColor_Crimson;
+        private System.Windows.Forms.ToolStripMenuItem tsmiForegroundColor_DarkKhaki;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBackgroundColor_MenuHighlight;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBackgroundColor_YellowGreen;
+        private System.Windows.Forms.ToolStripMenuItem tsmiBackgroundColor_Orange;
     }
 }
 
--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +21,22 @@ namespace Clock
                 Screen.PrimaryScreen.Bounds.Width - this.Width - 50,
                 50
                 );
+            // я не уверен что я сделал правильным способом но зато работает как надо
+            // BackgroundColor
+            tsmiBackgroundColor_MenuHighlight.Click += 
+                ((sender, e) => EventBodyBkColor(tsmiBackgroundColor_MenuHighlight, Color.DodgerBlue));
+            tsmiBackgroundColor_YellowGreen.Click += 
+                ((sender, e) => EventBodyBkColor(tsmiBackgroundColor_YellowGreen, Color.YellowGreen));
+            tsmiBackgroundColor_Orange.Click += 
+                ((sender, e) => EventBodyBkColor(tsmiBackgroundColor_Orange, Color.Orange));
+
+            // ForegroundColor
+            tsmiForegroundColor_ControlText.Click += 
+                ((sender, e) => EventBodyForeColor(tsmiForegroundColor_ControlText , Color.Black));
+            tsmiForegroundColor_Crimson.Click += 
+                ((sender, e) => EventBodyForeColor(tsmiForegroundColor_Crimson, Color.Crimson));
+            tsmiForegroundColor_DarkKhaki.Click += 
+                ((sender, e) => EventBodyForeColor(tsmiForegroundColor_DarkKhaki, Color.DarkKhaki));
         }
 
         private void timer_Tick(object sender, EventArgs e)
@@ -29,12 +46,38 @@ namespace Clock
                 "hh:mm:ss tt",
                 System.Globalization.CultureInfo.InvariantCulture
                 );
-            if (checkBoxShowDate.Checked)    labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+
+            if (checkBoxShowDate.Checked) labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
             if (checkBoxShowWeekDay.Checked) labelTime.Text += $"\n{DateTime.Now.ToString("ddd")}";
 
             notifyIcon.Text = labelTime.Text;
+
             
-            
+            this.TopMost = tsmiTopmost.Checked;
+            setVisibility(tsmiShowControls.Checked); // не уверен что хорошее решение
+
+            checkBoxShowDate.Checked = tsmiShowDate.Checked;
+            checkBoxShowWeekDay.Checked = tsmiShowWeekday.Checked;
+
+            if (tsmiExit.Checked) Close();
+        }
+        private void EventBodyBkColor(ToolStripMenuItem item , Color color) 
+        {
+            tsmiBackgroundColor_MenuHighlight.Checked = false;
+            tsmiBackgroundColor_YellowGreen.Checked = false;
+            tsmiBackgroundColor_Orange.Checked = false;
+
+            this.labelTime.BackColor = color;
+            item.Checked = true;
+        }
+        private void EventBodyForeColor(ToolStripMenuItem item , Color color) 
+        {
+            tsmiForegroundColor_ControlText.Checked = false;
+            tsmiForegroundColor_Crimson.Checked = false;
+            tsmiForegroundColor_DarkKhaki.Checked = false;
+
+            this.labelTime.ForeColor = color;
+            item.Checked = true;
         }
         void setVisibility(bool visible)
         {
@@ -46,14 +89,32 @@ namespace Clock
             this.TransparencyKey = visible ? Color.Empty : this.BackColor;          // Делаем окно прозначным.
             // Для того чтобы сделать окно прозрачным, его TransperencyKey должен совпадать с BackColor.
         }
+        
         private void buttonHideControls_Click(object sender, EventArgs e)
         {
             setVisibility(false);
+            tsmiShowControls.Checked = false;
         }
 
         private void labelTime_DoubleClick(object sender, EventArgs e)
         {
             setVisibility(true);
+            tsmiShowControls.Checked = true;
+        }
+
+        private void checkBoxShowDate_CheckedChanged(object sender, EventArgs e)
+        {
+            tsmiShowDate.Checked = checkBoxShowDate.Checked;
+        }
+
+        private void checkBoxShowWeekDay_CheckedChanged(object sender, EventArgs e)
+        {
+            tsmiShowWeekday.Checked = checkBoxShowWeekDay.Checked;
+        }
+
+        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            tsmiTopmost.Checked = true;
         }
     }
 }
